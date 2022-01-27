917bb29b5e38c7e4b05f7bdbae3dec9aa973d3d1
Eze Sandes <jezesandes@gmail.com>
2022-01-27 09:39:01 -0300

    feat: Added animated text
    fix: Added main photo
    
    fix: Changed projects imgs to a lower resolution

---
+++ js/typedText.js
@@ -0,0 +1,14 @@
+const options = {
+  strings: [
+    "[Aspiring] Computer Engenieer",
+    "Self-taught web developer ",
+    "Boca Juniors &#128153; &#128155; &#128153; fan",
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 1000,
+  startDelay: 400,
+  loop: true,
+};
+
+const typed = new Typed(".animate-subheading", options);
